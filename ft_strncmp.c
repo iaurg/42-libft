28b35ed5b03e9e771c7f0468a02bb9ebc62a9e49
Italo aurelio <italoaurelior@gmail.com>
2021-07-23 10:56:56 -0300

    Finish strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,15 @@
+int	ft_strncmp(const char *s1, const char *s2, unsigned int n)
+{
+	unsigned int	x;
+
+	x = 0;
+	while (x < n && (*s1 != '\0' || *s2 != '\0'))
+	{
+		if (*s1 != *s2)
+			return ((int)(unsigned char)*s1 - (int)(unsigned char)*s2);
+		s1++;
+		s2++;
+		x++;
+	}
+	return (0);
+}
