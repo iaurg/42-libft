b38f09504a4ef67fb95c5bbb6861fa54bfb55c08
Italo aurelio <italoaurelior@gmail.com>
2021-07-23 09:41:02 -0300

    Add new functions into header

--- libft.h
+++ libft.h
@@ -4,4 +4,8 @@ int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
+int	ft_isprint(int c);
+unsigned int	ft_strlen(char *c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
 #endif
