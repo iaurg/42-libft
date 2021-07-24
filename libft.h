90bf8e1b7b2faad5fce0b0ec4993d98b254377f3
Italo aurelio <italoaurelior@gmail.com>
2021-07-24 09:58:52 -0300

    Add new prototypes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@ int				ft_isdigit(int c);
 int				ft_isalnum(int c);
 int				ft_isascii(int c);
 int				ft_isprint(int c);
-unsigned int	ft_strlen(char *c);
+size_t			ft_strlen(char *c);
 int				ft_tolower(int c);
 int				ft_toupper(int c);
 int				ft_strncmp(const char *s1, const char *s2, size_t n);
@@ -14,4 +14,5 @@ void			*ft_memset(void *s, int c, size_t n);
 void			ft_bzero(void *s, size_t n);
 void			*ft_memcpy(void *dest, const void *src, size_t n);
 void			*ft_memmove(void *dest, const void *src, size_t n);
+size_t			ft_strlcpy(char *dst, const char *src, size_t size);
 #endif
