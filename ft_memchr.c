17b4c08c31ad8e5264c5863fa6aaae62b31f65fd
Italo aurelio <italoaurelior@gmail.com>
2021-07-24 19:15:01 -0300

    Finish memchr func

---
+++ ft_memchr.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+
+void	*ft_memchr(const void *b, int c, size_t len)
+{
+	unsigned char	*converted_b;
+
+	converted_b = (unsigned char *) b;
+	while (len--)
+	{
+		if (*converted_b == (unsigned char) c)
+			return ((void *)converted_b);
+		converted_b++;
+	}
+	return (NULL);
+}
+
+/*
+The memchr() function locates the first occurrence	of c (converted	to an
+unsigned char) in string b.
+
+The memchr() and memrchr()	functions return a pointer to the byte lo-
+cated, or NULL if no such byte exists within len bytes.
+*/
