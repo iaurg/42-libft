a217c19019a52af7379f41a9b41ccb2bf0511730
Italo aurelio <italoaurelior@gmail.com>
2021-07-24 16:17:27 -0300

    Add strchr into header

--- libft.h
+++ libft.h
@@ -16,4 +16,5 @@ void			*ft_memcpy(void *dst, const void *src, size_t n);
 void			*ft_memmove(void *dst, const void *src, size_t len);
 size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t			ft_strlcat(char *dst, const char *src, size_t dstsize);
+char			*ft_strchr(const char *s, int c);
 #endif
