aeeaf08803f0df8c78d69296d537549a3f669180
Italo aurelio <italoaurelior@gmail.com>
2021-07-25 14:56:35 -0300

    Refactor calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -2,12 +2,12 @@
 
 void	*ft_calloc(size_t number, size_t size)
 {
-	size_t	*ptr;
+	void	*ptr;
 
 	ptr = malloc(number * size);
 	if (!ptr)
 		return (NULL);
-	ft_bzero (ptr, size);
+	ft_bzero (ptr, number * size);
 	return (ptr);
 }
 
