f76429d9653048ad48109d16b7f71bf28d4349cc
Italo aurelio <italoaurelior@gmail.com>
2021-07-28 08:55:19 -0300

    Finish strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/28 07:43:37 by itaureli          #+#    #+#             */
+/*   Updated: 2021/07/28 08:54:56 by itaureli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new_string;
+	char	*hold_pointer;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (!s1 || !s2)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	new_string = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (!new_string)
+		return (NULL);
+	hold_pointer = new_string;
+	while (*s1)
+		*new_string++ = *s1++;
+	while (*s2)
+		*new_string++ = *s2++;
+	*new_string = '\0';
+	return (hold_pointer);
+}
