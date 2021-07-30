9cb5cc75c07b2bec9214b190510550c0b5c0c83c
Italo aurelio <italoaurelior@gmail.com>
2021-07-29 21:17:03 -0300

    Validate norminette

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 09:03:46 by itaureli          #+#    #+#             */
-/*   Updated: 2021/07/29 08:56:49 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/07/29 21:16:54 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	start;
 	size_t	end;
 
-	if(!s1 || !set)
+	if (!s1 || !set)
 		return (NULL);
-	if(!*s1 || !*set)
+	if (!*s1 || !*set)
 		return (ft_strdup(s1));
 	str_len = ft_strlen(s1) + 1;
 	start = 0;
@@ -32,26 +32,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 		end--;
 	return (ft_substr(s1, start, (end - start)));
 }
-/*
-int main(void)
-{
-	printf("3 - %s",ft_strtrim("tripouille   xxx", " x"));
-	printf("\n");
-	printf("5 - %s",ft_strtrim("   xxxtripouille   xxx", " x"));
-	printf("\n");
-	printf("11 - %s",ft_strtrim("123", ""));
-	printf("\n");
-	printf("%s",ft_strtrim("e111xcole2ee1", "c"));
-	printf("\n");
-	printf("%s",ft_strtrim("e111xcole2ee1", "e1"));
-	printf("\n");
-	printf("%s",ft_strtrim("111xcole2ee1", "1"));
-	printf("\n");
-	printf("%s",ft_strtrim("e111xcole2ee1", "e"));
-	printf("\n");
-	return 0;
-}
-*/
+
 /*
 string = "c eu sei o que acontece"
 
