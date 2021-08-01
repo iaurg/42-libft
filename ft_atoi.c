fb06b5ac533ead1bec52b8fe4f187ce4655eb8ee
Italo aurelio <italoaurelior@gmail.com>
2021-08-01 15:23:14 -0300

    Validate not int values

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 07:36:55 by itaureli          #+#    #+#             */
-/*   Updated: 2021/07/28 07:36:55 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/08/01 15:22:51 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,20 +28,27 @@ int	ft_atoi(const	char *nptr)
 {
 	int				final_number;
 	int				negative_signal;
+	int				int_counter;
 
 	final_number = 0;
-	negative_signal = 0;
+	negative_signal = 1;
 	while (ft_isspace(*nptr))
 		nptr++;
 	if (*nptr == '-' || *nptr == '+')
 	{
 		if (*nptr == '-')
-			negative_signal++;
+			negative_signal = -1;
 		nptr++;
 	}
+	int_counter = 0;
 	while (ft_isdigit(*nptr))
+	{
 		final_number = (final_number * 10) + to_n(*nptr++);
-	if (negative_signal % 2 != 0)
-		return (final_number * -1);
-	return (final_number);
+		int_counter++;
+	}
+	if (int_counter > 10 && negative_signal == 1)
+		return (-1);
+	else if (int_counter > 10)
+		return (0);
+	return (final_number * negative_signal);
 }
