01ffae72bf0151e809bd2d4cb31e192b14b54fb7
Italo aurelio <italoaurelior@gmail.com>
2021-08-02 08:41:47 -0300

    Create lst_addback func

---
+++ ft_lstadd_back.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/02 08:23:02 by itaureli          #+#    #+#             */
+/*   Updated: 2021/08/02 08:41:37 by itaureli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*lst_nav;
+
+	if (!new)
+		return ;
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	lst_nav = ft_lstlast(*lst);
+	lst_nav->next = new;
+}
