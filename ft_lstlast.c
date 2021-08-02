1a9badd33d44c2ef972b216189cb465e7cd92d4e
Italo aurelio <italoaurelior@gmail.com>
2021-08-02 08:16:50 -0300

    Create lstlat func

---
+++ ft_lstlast.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/02 08:08:25 by itaureli          #+#    #+#             */
+/*   Updated: 2021/08/02 08:16:29 by itaureli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*lst_position;
+	t_list	*last_lst;
+
+	lst_position = lst;
+	while (lst_position != NULL)
+	{
+		last_lst = lst_position;
+		lst_position = lst_position -> next;
+	}
+	return (last_lst);
+}
