04f98799d3783f0c08ba36b53512e2618d0cb261
Italo aurelio <italoaurelior@gmail.com>
2021-08-02 08:07:04 -0300

    Finish lst ize

---
+++ ft_lstsize.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/02 07:56:16 by itaureli          #+#    #+#             */
+/*   Updated: 2021/08/02 08:06:54 by itaureli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_lstsize(t_list *lst)
+{
+	int		lst_size;
+	t_list	*lst_nav;
+
+	lst_size = 0;
+	lst_nav = lst;
+	while (lst_nav != NULL)
+	{
+		lst_size++;
+		lst_nav = lst_nav->next;
+	}
+	return (lst_size);
+}
