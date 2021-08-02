83c98ec887e97a3c09c6d700368aecd556cf7565
Italo aurelio <italoaurelior@gmail.com>
2021-08-02 07:27:35 -0300

    create lstadd_front function

---
+++ ft_lstadd_front.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/02 07:16:15 by itaureli          #+#    #+#             */
+/*   Updated: 2021/08/02 07:27:16 by itaureli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	t_list	*temp_position;
+
+	temp_position = *lst;
+	*lst = new;
+	new->next = temp_position;
+}
